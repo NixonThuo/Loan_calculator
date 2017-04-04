d1307a0d32b4c52c10cdb103561f0fb869e635b1
NixonThuo <thuonixon808@gmail.com>
2017-04-03 20:18:36 -0700

    Second commit to rectify error

--- loan_amount.py
+++ loan_amount.py
@@ -1,12 +1,10 @@
 def loan(amount, rate, time):
-    while True:
-        try:
-            if time <= 12:
-                interest = amount * rate / 100 * time / 12
-                total_loan = interest + amount
-                return total_loan
-        except(AttributeError, ValueError, TypeError):
-            return "Invalid"
+    if time <= 12:
+        interest = amount * rate / 100 * time / 12
+        total_loan = interest + amount
+        return total_loan
+    else:
+        return "Invalid"
 
 
-print loan(10000, 12, 12)
+print loan(10000, 12, 14)
