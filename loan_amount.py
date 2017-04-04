346c12a29895146b60442c3bb685ece822121f00
NixonThuo <thuonixon808@gmail.com>
2017-04-04 00:53:25 -0700

    Commit with all tests

--- loan_amount.py
+++ loan_amount.py
@@ -1,5 +1,5 @@
 def loan(amount, rate, time):
-    if time <= 12:
+    if time <= 12 and all(isinstance(i, int) for i in [amount, rate, time]):
         interest = amount * rate / 100 * time / 12
         total_loan = interest + amount
         return total_loan
@@ -7,4 +7,4 @@ def loan(amount, rate, time):
         return "Invalid"
 
 
-print loan(10000, 12, 14)
+print loan(10000, 12, 5)
